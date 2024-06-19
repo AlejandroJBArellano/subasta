dab5095011c3d24dbb49a6f70453165e51f3f5cc
Alejandro Jesús Arellano <jesusarellano334@gmail.com>
2024-06-18 17:48:37 -0700

    xd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {onAuthUIStateChange,AuthState,CognitoUserInterface} from "@aws-amplify/ui-components"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  subastaActiva: Boolean = true;
   title = 'subasta';
+  user: CognitoUserInterface | undefined
+
+  subasta = {
+    title: "",
+    description: "",
+    urlImage: ""
+  }
+
 }
